79144dac493a9a28d50ff8febbbb16dc148f458b
webwithsumit <sumit2410kushwaha@gmail.com>
2024-10-16 21:41:58 +0530

    program second for adding and printing

---
+++ src/Q2_sumPrint.java
@@ -0,0 +1,9 @@
+
+
+public class Q2_sumPrint {
+    public static void main(String[] args) {
+        int a = 45, b = 12;
+        int sum = a + b;
+        System.out.println("The sum " + a + " + " + b+ " = " + sum );
+    }
+}
